5f18457200efd055d2eb45ece9621002fa61e962
Mitko Donchev <roked122@gmail.com>
2020-10-22 09:58:18 +0100

    Latest updates: now the user can send files to channels which the bot is part of. Also, the bot can send messages/files to private channels after it is invited.

--- middleware/middlewares.js
+++ middleware/middlewares.js
@@ -12,7 +12,7 @@ const {WebClient} = pkg;
 
 //Read the slack bot token from the environment variables
 //TODO - remove token from here ( it can't be sore in .env when using Webstorm )
-const slackToken = process.env.SLACK_TOKEN || 'xoxb-1455276216688-1431663829427-utgeq4KaEwI45rAhUOuweT31';
+const slackToken = process.env.SLACK_TOKEN || '';
 
 //Initialize web client
 const web = new WebClient(slackToken)
